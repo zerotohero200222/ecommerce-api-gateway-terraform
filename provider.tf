ec7200fc42b224ff6ff41c3f4b161bdd45605e6a
zerotohero200222 <aebharath17@gmail.com>
2026-03-18 07:17:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,9 +14,9 @@ terraform {
       source  = "hashicorp/google-beta"
       version = "~> 5.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.4"
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
     }
   }
 
